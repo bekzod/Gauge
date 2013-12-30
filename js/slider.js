8b72601fd4680848058887eeec03f15abdd7c5e7
Bek Khaitbaev <bek.khaitbaev@onetwotrip.com>
2013-12-30 15:29:48 +0400

    WIP

--- js/slider.js
+++ js/slider.js
@@ -8,7 +8,6 @@ $(function () {
     function Plugin ( element, options ) {
         this.$el = $(element).wrapInner('<div class = "mega-slider"/>');
         this.$slidesEl = this.$el.find('.mega-slide-element');
-        console.log(this.$slidesEl.eq(0).width());
 
         this.settings = $.extend( {}, defaults, options );
         this._defaults = defaults;
@@ -22,25 +21,34 @@ $(function () {
         init: function () {
             var self = this;
             var viewportWidth = this.$el.width();
+            console.log(viewportWidth);
             var currentSlideWidth = 0;
             var slidesIndex = [];
 
             this.$slidesEl.each( function(index, el){
                 var $el = $(el);
-                if( currentSlideWidth - $el.width() <= 0 ){
+                if( currentSlideWidth - $el.outerWidth() <= 0 ){
                     currentSlideWidth = viewportWidth;
                     slidesIndex.push([]);
                 }
-                currentSlideWidth -= $el.width();
+                currentSlideWidth -= $el.outerWidth();
                 slidesIndex[ slidesIndex.length - 1 ].push( index );
             });
 
+            console.log(slidesIndex);
+
             for (var i = slidesIndex.length - 1; i >= 0; i--) {
-                this.$slidesEl
-                    .filter( function(index,el){
-                        return $.inArray(index, slidesIndex[i]) > -1;
-                    })
-                    .wrapAll('<div class = "mega-slide" />');
+                var $slide = this.$slidesEl
+                            .filter( function(index,el){
+                                return $.inArray(index, slidesIndex[i]) > -1;
+                            })
+                            .wrapAll('<div class = "mega-slide" />')
+                            .parent();
+                var remainingWidth = this.$el.width() - $slide.width();
+                $slide.css({
+                    'margin-right':remainingWidth/2,
+                    'margin-left':remainingWidth/2
+                });
             }
 
             this.$slides = this.$el.find('.mega-slide');
@@ -99,11 +107,9 @@ $(function () {
                 this.currentIndex = this.$slides.length - 1;
             }
 
-            this.$el
+            this.$el.children().first()
                 .stop(true)
-                .animate({
-                    'left': -(this.currentIndex * this.$el.width())
-                });
+                .animate({scrollLeft:this.$el.width()*this.currentIndex });
 
             this.$indicator
                 .find('.dot')
